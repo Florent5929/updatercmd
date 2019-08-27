a652c6409feecd1efd9c772908d956981ac6a068
florent <florent@florent-Z390-UD>
2019-08-27 22:46:15 +0200

    .

--- src/com/updatercmd/UpdatercmdCommandExecutor.java
+++ src/com/updatercmd/UpdatercmdCommandExecutor.java
@@ -1,6 +1,7 @@
 package com.updatercmd;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.block.CommandBlock;
 import org.bukkit.command.Command;
@@ -21,7 +22,7 @@ public class UpdatercmdCommandExecutor implements CommandExecutor {
 			if(!player.isOp()){
 				player.sendMessage(ChatColor.RED + "Commande opérateur uniquement !");
 			} else {
-				ExecuteCommand(player);
+				ExecuteCommand(player, args);
 				return true;
 			}
 		}
@@ -31,8 +32,21 @@ public class UpdatercmdCommandExecutor implements CommandExecutor {
 	}
 	
 	
-	public void ExecuteCommand(Player player) {
-		int radius = 20;
+	public void ExecuteCommand(Player player, String[] args) {
+		int radius = 0;
+		
+		try {
+			if(args.length != 0){
+				radius = Integer.parseInt(args[0]);
+			}
+		} catch (Exception e) {
+			
+		} finally {
+			if(radius <= 0) {
+				radius = 20;
+			}	
+		}
+		
 		Block playerBlock = player.getLocation().getBlock();
 		int cmdBlocks = 0;
 		
@@ -57,7 +71,7 @@ public class UpdatercmdCommandExecutor implements CommandExecutor {
 		  
 		}
 		
-		player.sendMessage(ChatColor.YELLOW + "Vous avez mis à jour " + cmdBlocks + " command blocks.");
+		player.sendMessage(ChatColor.YELLOW + "Vous avez mis à jour " + cmdBlocks + " command blocks.\nVoir les logs pour plus de détails.");
 		Plugin.LOG.info(cmdBlocks + " command blocks ont été modifiés.");
 		
 	}
@@ -66,19 +80,45 @@ public class UpdatercmdCommandExecutor implements CommandExecutor {
 		String oldcmd = cmdblock.getCommand();
 		String newcmd = oldcmd;
 		
-		if(oldcmd.startsWith("execute") || oldcmd.startsWith("/execute")) {
-			newcmd = oldcmd.replaceAll("execute @", "execute as @");
-			newcmd = newcmd.replaceAll("r=","distance=");
-			newcmd = newcmd.replaceAll("~ ~ ~ /","run ");
+
+		newcmd = oldcmd.replaceAll("execute @", "execute as @");
+		newcmd = newcmd.replaceAll("r=","distance=..");
+		newcmd = newcmd.replaceAll("~ ~ ~ /","run ");
+		newcmd = newcmd.replaceAll("type=Player","type=player");
+		
+		if(newcmd.matches(".+minecraft:[a-zA-Z_]+ [0-9] [0-9]$")){
+			// Si contient un ou plusieurs caractères suivi de minecraft: suivi d'un ou plusieurs caractères alphabétiques (symbole _ accepté)
+			// suivi d'un espace, suivi d'un chiffre, suivi d'un autre espace, suivi d'un autre chiffre
+			// puis suivi de la fin de la chaîne
+			// alors on supprime le dernier chiffre et le dernier espace
+			
+			newcmd = newcmd.substring(0, newcmd.length()-2);		
 		}
 		
+		
 		if(oldcmd.contains("setblock")){
-			newcmd = newcmd.replaceAll("_button 0", "_button[face=ceiling]");
-			newcmd = newcmd.replaceAll("_button 1", "_button[facing=east]");
-			newcmd = newcmd.replaceAll("_button 2", "_button[facing=west]");
-			newcmd = newcmd.replaceAll("_button 3", "_button[facing=south]");
-			newcmd = newcmd.replaceAll("_button 4", "_button[face=floor]");
+			newcmd = newcmd.replaceAll("wooden_button 0", "oak_button[face=ceiling]");
+			newcmd = newcmd.replaceAll("wooden_button 1", "oak_button[facing=east]");
+			newcmd = newcmd.replaceAll("wooden_button 2", "oak_button[facing=west]");
+			newcmd = newcmd.replaceAll("wooden_button 3", "oak_button[facing=south]");
+			newcmd = newcmd.replaceAll("wooden_button 4", "oak_button[face=floor]");
+			
+			newcmd = newcmd.replaceAll("minecraft:stone 0", "minecraft:stone");
+			newcmd = newcmd.replaceAll("minecraft:stone 1", "minecraft:granite");
+			newcmd = newcmd.replaceAll("minecraft:stone 2", "minecraft:polished_granite");
+			newcmd = newcmd.replaceAll("minecraft:stone 3", "minecraft:diorite");
+			newcmd = newcmd.replaceAll("minecraft:stone 4", "minecraft:polished_diorite");
+			newcmd = newcmd.replaceAll("minecraft:stone 5", "minecraft:andesite");
+			newcmd = newcmd.replaceAll("minecraft:stone 6", "minecraft:polished_andesite");
 		}
+		
+		if(oldcmd.contains("fill")){
+			newcmd = newcmd.replaceAll("minecraft:air 0 replace", "minecraft:air replace");
+		}
+		
+		//if(oldcmd.contains("particle")){
+		//	newcmd = newcmd.replaceAll("particle blockcrack", "particle block oak_planks");
+		//}
 		
 		if(!oldcmd.equals(newcmd)) {
 			cmdblock.setCommand(newcmd);
@@ -86,6 +126,8 @@ public class UpdatercmdCommandExecutor implements CommandExecutor {
 			String alert = ChatColor.GOLD + "Cmd block modifié de : " + ChatColor.WHITE + oldcmd + ChatColor.GOLD + " à : " + ChatColor.WHITE + newcmd;
 			player.sendMessage(alert);
 			Plugin.LOG.info("Cmd block modifié de : " + oldcmd + " à : " + newcmd);
+			Location loc = player.getLocation();
+			Plugin.LOG.info("Location : x=" + loc.getX() + ", y=" + loc.getY() + ", z=" + loc.getZ());
 			return true;
 		} else {
 			return false;
